eb0bcb646a53552fd5d7f5df5c2087f08e15b5a5
amr-daoud <amr.f.daoud@gmail.com>
2024-09-24 12:18:20 +0300

    InitialCommit

---
+++ api/Models/Car.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ChassisNumber { get; set; }
+    }
+}
